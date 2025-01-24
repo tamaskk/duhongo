8399b52314ccbf5eae4867b1ddc702962d8cdde3
kalmantomika <tamas@blcks.io>
2025-01-24 16:31:29 +0100

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-  // return (
-  //   <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
-  //     <h1>Coming soon...</h1>
-  //   </div>
-  // )
+  // return <Component {...pageProps} />;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <h1>Coming soon...</h1>
+    </div>
+  )
 }
